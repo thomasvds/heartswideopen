0a150a5721d0c6251e165b7b24f4b9657465c809
Thomas Vanderstraeten <vanderstraeten.thomas@gmail.com>
2017-10-21 18:29:39 +0200

    Doen

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['redis_to_go_url'] }
+  config.redis = { url: ENV['REDISTOGO_URL'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['redis_to_go_url'] }
+  config.redis = { url: ENV['REDISTOGO_URL'] }
 end
